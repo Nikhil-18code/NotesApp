a0ba53f8e6e09a07b4b283569eacf0132f5af46c
Nikhil-18code <77283581+Nikhil-18code@users.noreply.github.com>
2022-07-31 19:03:13 +0530

    udated notes to the local storage
    Earlier the web app use to clear all the notes as soon as refreshed or on starting that on a new server....
    This update (on line 25 and line 8) improves the shorcoming

--- components/App.jsx
+++ components/App.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+//Getting data from local storage
+const getLocalData=()=>{
+    const notes=localStorage.getItem('notes');
+    if(notes){
+      return JSON.parse(notes);
+    }
+    else{
+      return [];
+    }
+  }
 function App() {
-    const [notes,setNotes]=useState([]);
+    const [notes,setNotes]=useState(getLocalData());
     function addNote(newNote){
        setNotes(prevNote=>{
          return [...prevNote,newNote]
        })
     }
-
+    //Adding to Locale Storage
+    useEffect(() =>{
+      localStorage.setItem('notes',JSON.stringify(notes))
+     },[notes]);
+     
     function deleteNote(id){
         setNotes(prevNote=>{
             return prevNote.filter((noteItem,index)=>{
